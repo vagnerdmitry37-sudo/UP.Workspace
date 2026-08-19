0ab7d844f34cfdaf047355093e0ab5c7c95143ea
Vagner Dmitry <dmitry.vagner@tui.co.uk>
2026-08-19 10:57:45 +0200

    resolve lib deps

--- projects/up-admin/src/app/app.config.ts
+++ projects/up-admin/src/app/app.config.ts
@@ -4,7 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideUpAngularUI } from '@up-angular-ui/core';
+import { provideOptimus } from '@openng/optimus-ui/config';
+import Aura from '@openng/optimus-ui-themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideUpAngularUI(), provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideUpAngularUI(),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideOptimus({ theme: { preset: Aura } }),
+  ],
 };
--- projects/up-angular-ui/src/lib/tools/up-provider.ts
+++ projects/up-angular-ui/src/lib/tools/up-provider.ts
@@ -1,30 +1,11 @@
 import { provideOptimus } from '@openng/optimus-ui/config';
 import Aura from '@openng/optimus-ui-themes/aura';
-import Lara from '@openng/optimus-ui-themes/lara';
-import Material from '@openng/optimus-ui-themes/material';
-import Nora from '@openng/optimus-ui-themes/nora';
-import { Preset } from '@openng/optimus-ui-themes/types';
 import { DARK_MODE_SELECTOR } from '../constants';
 
-type UpPreset = 'Aura' | 'Lara' | 'Material' | 'Nora';
-
-interface ProvideUpAngularUIProps {
-  preset?: UpPreset;
-}
-
-const presetsMap: Record<UpPreset, Preset> = {
-  Aura,
-  Lara,
-  Material,
-  Nora,
-};
-
-export const provideUpAngularUI = (props: ProvideUpAngularUIProps = {}) => {
-  const { preset = 'Aura' } = props;
-
+export const provideUpAngularUI = () => {
   return provideOptimus({
     theme: {
-      preset: presetsMap[preset],
+      preset: Aura,
       options: { darkModeSelector: `.${DARK_MODE_SELECTOR}` },
     },
   });
